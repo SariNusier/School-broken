8d5fa0b0f73058023e37a0d12f6af5a1da3a138d
Sari Nusier <sari.nusier@gmail.com>
2016-10-28 22:39:23 +0100

    Some changes to delete.sql before rename

--- DB/Courseworks/Coursework1/part1/temp/delete_temp.sql
+++ DB/Courseworks/Coursework1/part1/temp/delete_temp.sql
@@ -6,6 +6,29 @@
 
 
 -- add your DELETE statements here
+
+-- FIRST WE FIND THE MOST CONTROVERSIAL EMPLOYEE
+SET @controversialEmail = (
+  SELECT `employee`.`email`
+  FROM (
+	   SELECT DISTINCT
+        (`creatorId`) AS `creatorId`,
+        (SELECT COUNT(*)
+         FROM `comment`
+         WHERE `comment`.`statusId` = `status`.`id`
+				) AS `count`
+      FROM `status`
+      HAVING `count` =
+	 		  (SELECT COUNT(*)
+          FROM `comment`
+          GROUP BY `statusId`
+          ORDER BY `COUNT(*)` DESC
+          LIMIT 1
+        )
+  ) AS allTable
+  JOIN `employee` ON `employee`.`id` = `creatorId` LIMIT 1
+);
+
 SET SQL_SAFE_UPDATES=0;
-DELETE FROM `employee` WHERE `email` = 'liam@neeson.com';
+DELETE FROM `employee` WHERE `email` = @controversialEmail;
 SET SQL_SAFE_UPDATES=1;
