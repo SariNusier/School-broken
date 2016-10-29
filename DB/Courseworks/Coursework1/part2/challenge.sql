c427106463e90700a7164ee152d96cf551bdc861
Sari Nusier <sari.nusier@gmail.com>
2016-10-29 16:47:35 +0100

    challenge done

--- DB/Courseworks/Coursework1/part2/challenge.sql
+++ DB/Courseworks/Coursework1/part2/challenge.sql
@@ -11,31 +11,26 @@
 ALTER TABLE `status`
 ADD COLUMN `attachment` LONGBLOB NULL;
 
--- One table is created for both allowing event creators to invite people, and
--- allowing any employee to confirm coming to an event.
--- This is achieved by having a nullable BIT field `confirmed`. An employee is allowed
--- to create invitations only if they've created the event, OR if they invite themselves.
--- If an employee invites themselves, the confirmed field MUST be 1, thus allowing
--- for any employee to confirm attendence to an event.
+-- Invitation table is used to store the invitations sent for specific events.
+-- A trigger implemented below makes sure that the invitor id is the as the event creatorId
 CREATE TABLE `invitation` (
   `invitorId` INT NOT NULL,
   `inviteeId` INT NOT NULL,
   `eventId` INT NOT NULL,
-  `confirmed` BIT NULL,
   PRIMARY KEY (`invitorId`, `inviteeId`, `eventId`),
   CONSTRAINT `fk_invitation_invitor`
     FOREIGN KEY (`invitorId`)
-    REFERENCES `k1333307db`.`employee` (`id`)
+    REFERENCES `employee` (`id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `fk_invitation_invitee`
     FOREIGN KEY (`inviteeId`)
-    REFERENCES `k1333307db`.`employee` (`id`)
+    REFERENCES `employee` (`id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `fk_invitation_event`
     FOREIGN KEY (`eventId`)
-    REFERENCES `k1333307db`.`event` (`id`)
+    REFERENCES `event` (`id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
@@ -45,7 +40,7 @@ CREATE TABLE `attendance` (
   `eventId` INT NOT NULL,
   PRIMARY KEY (`attendeeId`, `eventId`),
   CONSTRAINT `fk_attendance_attendee`
-    FOREIGN KEY(`invitorId`)
+    FOREIGN KEY(`attendeeId`)
     REFERENCES `employee` (`id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
@@ -56,32 +51,16 @@ CREATE TABLE `attendance` (
     ON UPDATE CASCADE
 );
 
--- The trigger makes sure that only event creators can invite others OR
--- if an employee invites themselves, the confirmed status is set to 1 by default.
+-- The trigger makes sure that only event creators can invite others.
 -- Otherwise, display an error.
 delimiter $$
-  create trigger `trigger_before_insert_invitation` before insert on `invitation`
+  CREATE TRIGGER `trigger_before_insert_invitation` BEFORE INSERT ON `invitation`
   for each row
   begin
       declare error_msg varchar(128);
-      if new.invitorId = new.inviteeId then
-  		SET NEW.confirmed = 1;
-  	elseif new.invitorId != (SELECT `creatorId` FROM `event` WHERE `id` = NEW.eventId) THEN
+  	  if new.invitorId != (SELECT `creatorId` FROM `event` WHERE `id` = NEW.eventId) THEN
           set error_msg = 'The event was not created by the invitor';
           signal sqlstate '45000' set message_text = error_msg;
       end if;
   end$$
-
--- The trigger makes sure that the confirmed status cannot be updated to NULL,
--- once it has been set to 0 or 1.
-  create trigger `trigger_before_update_invitation` before update on `invitation`
-    for each row
-    begin
-        declare error_msg varchar(128);
-        if new.confirmed IS NULL then
-            set error_msg = 'Cannot set confirmed value to NULL';
-            signal sqlstate '45000' set message_text = error_msg;
-        end if;
-    end$$
-
 delimiter ;
