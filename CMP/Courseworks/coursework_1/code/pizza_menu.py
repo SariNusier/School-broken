7c917271a1e215f0b2e9a8445cea3520c2e54273
Sari Nusier <sari.nusier@gmail.com>
2016-11-01 18:18:26 +0000

    done with pizza_menu.py

--- CMP/Courseworks/coursework_1/code/pizza_menu.py
+++ CMP/Courseworks/coursework_1/code/pizza_menu.py
@@ -1,28 +1,54 @@
+"""
+Sari Nusier
+1317015
+
+The script takes as input the pizza.xml file which describes the pizza menu of a
+specific restaurant. It then outputs the menu (to stdout) in the following format:
+
+# (RestaurantName)
+
+## Sizes
+- (size1)
+- (size2)
+...
+
+## Toppings
+- (topping1)
+- (topping2)
+...
+
+## Crusts
+- (crust1)
+- (crust2)
+...
+
+"""
+
 import xml.etree.ElementTree as et
 
 
 try:
     # reading file and setting root of xml tree
-    tree = et.ElementTree(file='data/pizza.xml')
-    root = tree.getroot()
+    pizza_menu_tree = et.ElementTree(file='data/pizza.xml')
+    pizza_menu_root = pizza_menu_tree.getroot()
 
     # printing shop name
-    print("# %s\n"%root.find("shopname").text)
+    print("# %s\n"%pizza_menu_root.find("shopname").text)
 
     # printing sizes
     print("## Sizes")
-    for size in root.find("sizes"):
+    for size in pizza_menu_root.find("sizes"):
         print("- %s"%size.text)
 
     # printing toppings
     print("\n## Toppings")
-    for topping in root.find("toppings"):
+    for topping in pizza_menu_root.find("toppings"):
         print("- %s"%topping.text)
 
     # printing crusts
     print("\n## Crusts")
-    for crust in root.find("crusts"):
+    for crust in pizza_menu_root.find("crusts"):
         print("- %s"%crust.text)
-        
+
 except IOError as ioe:
     print("I/O Error: %s" % ioe)
