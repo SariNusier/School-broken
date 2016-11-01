eca76171095d7b06124c4a292a129d1b446e6457
Sari Nusier <sari.nusier@gmail.com>
2016-11-01 18:27:05 +0000

    pizza_report.py done

--- CMP/Courseworks/coursework_1/code/pizza_report.py
+++ CMP/Courseworks/coursework_1/code/pizza_report.py
@@ -1,31 +1,45 @@
+"""
+Sari Nusier
+1317015
+
+Description: Solution to Coursework 1 Part 2
+
+Input: data/pizza.xml file which describes the pizza menu of a specific
+restaurant.
+
+Output: data/pizza_report.csv file containing the number of available pizza
+sizes, toppings, crusts and the total number of possible pizzas that can be
+created.
+"""
+
 import xml.etree.ElementTree as et
 import csv
 
 
 try:
     # reading file and setting root of xml tree
-    tree = et.ElementTree(file='data/pizza.xml')
-    root = tree.getroot()
+    pizza_menu_tree = et.ElementTree(file='data/pizza.xml')
+    pizza_menu_root = pizza_menu_tree.getroot()
 
     # totals are stored into a dictionary
-    result = {}
+    totals = {}
 
     # number of sizes available
-    result["sizes"] = len(root.find("sizes"))
+    totals["sizes"] = len(pizza_menu_root.find("sizes"))
 
     # number of toppings available
-    result["toppings"] = len(root.find("toppings"))
+    totals["toppings"] = len(pizza_menu_root.find("toppings"))
 
     # number of crusts available
-    result["crusts"] = len(root.find("crusts"))
+    totals["crusts"] = len(pizza_menu_root.find("crusts"))
 
     # number of possible pizzas
-    result["total_combo"] = result["sizes"]*(2**result["toppings"])*result["crusts"]
+    totals["total_combo"] = totals["sizes"]*(2**totals["toppings"])*totals["crusts"]
 
     # writing the results into the csv file
     with open("data/pizza_report.csv", "w") as csvFile:
         writer = csv.DictWriter(csvFile, fieldnames=['sizes','toppings','crusts','total_combo'])
         writer.writeheader()
-        writer.writerow(result)
+        writer.writerow(totals)
 except IOError as ioe:
     print("I/O Error: %s" % ioe)
