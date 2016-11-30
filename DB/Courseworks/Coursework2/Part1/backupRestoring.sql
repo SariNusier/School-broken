08ae38661f35179f54f1ec7e2e93e516d6e6bbce
Sari Nusier <sari.nusier@gmail.com>
2016-11-30 21:02:30 +0000

    Work on DB coursework2

--- DB/Courseworks/Coursework2/Part1/backupRestoring.sql
+++ DB/Courseworks/Coursework2/Part1/backupRestoring.sql
@@ -5,3 +5,10 @@
 
 
 -- add your Backup Restoring statements here
+
+-- connecting to calcium:
+-- ssh k1333307@calcium.inf.kcl.ac.uk
+
+-- Restoring database:
+-- mysql -u k1333307 -p -h nmsdvm999956.nms.kcl.ac.uk k1333307db < crimes2015.sql
+SOURCE crimes2015.sql;
